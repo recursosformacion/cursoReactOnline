71bec23a9a8f1abb7804304420855f28c25f4e73
miguel <migarcia@dopc.com>
2022-04-07 17:07:13 +0200

    todo-1

--- mp3_login/src/notas.js
+++ mp3_login/src/notas.js
@@ -26,4 +26,37 @@ class Notas extends Component {
             <Mitag story={ props.story } />
         </duv>
     }
+
+
+    pp() {
+        let usuarioLogonado=false
+
+
+        if (usuarioLogonado) {
+            return <PantallaLogout />
+        }
+        return <PantallaLogin />
+
+
+        let pantalla;
+        if (usuarioLogonado) {
+            pantalla = < PantallaLogout />
+        }
+        else {
+            pantalla = < PantallaLogin />
+        }
+        return (
+            <div >
+                <h1 > Pruebas </h1> 
+                { pantalla } 
+            </div>
+            )
+            {usuarioLogonado?
+                <PantallaLogout/>
+                :
+                <PantallaLogin/>
+            }
+    
+    
+    }​
 }
